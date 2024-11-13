d9cd23f5a8f31f1366055daa9d17b2981bf72de1
Dominik <dominik.hezel@em.uni-frankfurt.de>
2024-11-13 16:20:30 +0100

    x

--- model.py
+++ model.py
@@ -11,7 +11,6 @@ from matplotlib.patches import Rectangle
 
 st.title('2D/3D model')
 
-
 tab1, tab2, tab3 = st.tabs(['explore', 'run a model', 'switch'])
 # the mu of the parent 3D distribution is fixed at a typical value for chondrule size distributions
 # taken from the metzler data fits below
@@ -26,75 +25,85 @@ with tab1:
         max_chd_size = st.selectbox('max. chd. diameter', df_results['max. Chd Diameter'].unique(), index=1)
     with col2:
         sel_mu3D = st.selectbox('initial $\mu$ 3D', df_results['initial $\mu$ 3D'].unique(), index=1)
+    
+    utils.func.model_plot(max_chd_size, sel_mu3D, df_results)
 
-    fil = (df_results['max. Chd Diameter'] == max_chd_size) & (df_results['initial $\mu$ 3D'] == sel_mu3D)
-    dfParameters = df_results[fil]
+with tab2:    
+    col1, col2, col3 = st.columns(3)
+    with col1:
+        mu3DStart = st.number_input('start', value=6.2, step=.2, key='mu3Dstart')
+    with col2:
+        mu3DSEnd = st.number_input('end', value=6.4, step=.2, key='mu3Dend')
+    with col3:
+        mu3Dstep = st.number_input('steps', value=.2, step=.2, key='mu3Dstep')
+    mu3DList = [x/10 for x in range(int(mu3DStart*10), int((mu3DSEnd+.2)*10), int(mu3Dstep*10))]
+    mu3DList.insert(0, 'µ 3D')
 
-    filBlue = dfParameters['mean 2D'] <= dfParameters['mean 3D']  # 2D < 3D
-    filOrange = dfParameters['mean 2D'] > dfParameters['mean 3D']  # 2D > 3D
+    col1, col2, col3 = st.columns(3)
+    with col1:
+        iniSigmastart = st.number_input('start', value=.4, step=.05, key='iniSigmastart')
+    with col2:
+        iniSigmaend = st.number_input('end', value=.6, step=.05, key='iniSigmaend')
+    with col3:
+        iniSigmastep = st.number_input('steps', value=.1, step=.1, key='iniSigmastep')
+    iniSigma_List = [x/10 for x in range(int(iniSigmastart*10), int((iniSigmaend+.1)*10), int(iniSigmastep*10))]
+    mu3DList.insert(0, 'initial ')
 
-    plt.scatter(dfParameters[filBlue][r'initial $\sigma$ 3D'], dfParameters[filBlue]['min. Chd Diameter'], color = 'royalblue', s = 100)
-    plt.scatter(dfParameters[filOrange][r'initial $\sigma$ 3D'], dfParameters[filOrange]['min. Chd Diameter'], color = 'sandybrown', s = 100)
+    col1, col2, col3 = st.columns(3)
+    with col1:
+        minChdSize_rangestart = st.number_input('start', value=100, step=50, key='minChdSize_rangestart')
+    with col2:
+        minChdSize_rangeend = st.number_input('end', value=200, step=50, key='minChdSize_rangeend')
+    with col3:
+        minChdSize_rangestep = st.number_input('steps', value=50, step=50, key='minChdSize_rangestep')
+    minChdSize_range = [x/10 for x in range(int(minChdSize_rangestart*10), int((minChdSize_rangeend+.1)*10), int(minChdSize_rangestep*10))]
+    mu3DList.insert(0, 'µ 3D')
 
-    plt.scatter([.53, .46, .44],[90, 180, 245], color = 'black', edgecolors= 'white', linewidths = 3, s = 250)
+    col1, col2, col3 = st.columns(3)
+    with col1:
+        maxChdSize_rangestart = st.number_input('start', value=1500, step=100, key='maxChdSize_rangestart')
+    with col2:
+        maxChdSize_rangeend = st.number_input('end', value=2000, step=100, key='maxChdSize_rangeend')
+    with col3:
+        maxChdSize_rangestep = st.number_input('steps', value=500, step=100, key='maxChdSize_rangestep')
+    maxChdSize_range = [x/10 for x in range(int(maxChdSize_rangestart*10), int((maxChdSize_rangeend+.1)*10), int(maxChdSize_rangestep*10))]
+    mu3DList.insert(0, 'µ 3D')
+   
+    col1, col2 = st.columns(2)
+    with col1:
+        numberOfChondrules_power = st.number_input('nr. of chondrules (10^x)', value=5, step=1)
+    with col2:
+        zAxisLength_power = st.number_input('length of z-axis (10^x)', value=4, step=1)
+    numberOfChondrules = 10**numberOfChondrules_power
+    zAxisLength = 10**zAxisLength_power
 
-    plt.xlabel(r'$\sigma$ of the 3D distribution')
-    plt.ylabel('minimum sphere diameter (µm)')
 
-    # plt.savefig('Fig. 3.pdf')
+    st.dataframe(pd.DataFrame([
+        mu3DList,
+        iniSigma_List,
+        minChdSize_range,
+        maxChdSize_range,
+        [numberOfChondrules, zAxisLength]
+        ]))
 
-    st.pyplot(plt)
+
+    if st.button('calculate'):
+            utils.func.run_model(mu3DList, iniSigma_List, minChdSize_range, maxChdSize_range, numberOfChondrules, zAxisLength)
+            # st.write('finished')
+
+    with st.expander('Results Table'):
+        st.dataframe(st.session_state.dfParameters)
 
 
-with tab2:
-    mu3DList = [6.2, 6.4] # [6, 6.2, 6.4, 6.6] # mu3DList = [x/100 for x in range(500,720,25)]
-    iniSigma_List = [x/100 for x in range(40,60,5)] # [x/100 for x in range(5,105,5)]
-    minChdSize_range = range(100, 200, 50) # range(0, 420, 20)
-    maxChdSize_range = [1500, 2000] #[1000, 1500, 2000, 4000]
-    numberOfChondrules = 10**5 # 10**6
-    zAxisLength = 10**4 # 10**5
-    parameters = []
+    df_results_own = st.session_state.dfParameters
 
-    st.subheader('run a model')
+    col1, col2 = st.columns(2)
+    with col1:
+        max_chd_size_own_model = st.selectbox('max. chd. diameter', df_results_own['max. Chd Diameter'].unique(), index=1, key='max_chd_size_own_model')
+    with col2:
+        sel_mu3D_own_model = st.selectbox('initial $\mu$ 3D', df_results_own['initial $\mu$ 3D'].unique(), index=1, key='sel_mu3D_own_model')
 
-    # tot_loops = len(maxChdSize_range) * len(mu3DList) * len(iniSigma_List) * len(minChdSize_range)
-    # model_progress_bar = st.progress(0)
-    # loop_counter = 0
-
-    # for maxChdSize in maxChdSize_range:
-    #     for mu3D in mu3DList:
-    #         # the minimum chondrule diameters as well as the sigma of the chondrule size distribution are varied
-    #         for iniSigma in iniSigma_List:
-    #             # clear_output(wait=True) # for progress indicator, which seems not to be working in streamlit
-    #             # st.write(f'max Chd Size: {maxChdSize}')
-    #             # st.write(f'mu3D: {mu3D}')
-    #             # st.write(f'remaining iniSigma loops:  {len(iniSigma_List)-loop_counter}')
-    #             dfChdList = utils.func.chd3DList(numberOfChondrules, zAxisLength, mu3D, iniSigma) # noc, zAxisLen, mu, sigma
-                
-    #             for minChdSize in minChdSize_range:
-    #                 filDiameter = (dfChdList['Chd Diameter'] > minChdSize) & (dfChdList['Chd Diameter'] < maxChdSize)
-    #                 dfChdList = dfChdList[filDiameter]
-    #                 appChdDiameterList = utils.func.sectionedChd(0.5 * zAxisLength, dfChdList)
-        
-    #                 muFit3D, SigmaFit3D = utils.func.calcMuSigma(dfChdList['Chd Diameter'])
-    #                 muFit2D, sigmaFit2D = utils.func.calcMuSigma(appChdDiameterList)
-        
-    #                 parameters3D = [muFit3D, SigmaFit3D, np.e**(muFit3D + .5 * SigmaFit3D**2), np.e**muFit3D, np.e**(muFit3D - SigmaFit3D**2)]   # 3D: mu, sigma, mean, median, mode
-    #                 parameters2D = [muFit2D, sigmaFit2D, np.e**(muFit2D + .5 * sigmaFit2D**2), np.e**muFit2D, np.e**(muFit2D - sigmaFit2D**2)]
-    #                 parameters.append([len(appChdDiameterList), minChdSize, maxChdSize, mu3D, iniSigma] + parameters3D + parameters2D)
-    #                 loop_counter+=1
-    #                 model_progress_bar.progress(loop_counter/tot_loops, text='calculating – this may take some time')
-    # model_progress_bar.empty()
-    # dfParameters = pd.DataFrame(parameters)
-
-    # dfParameters = dfParameters.rename(columns = {0:'Nr. of sect. Chd', 1:'min. Chd Diameter', 2:'max. Chd Diameter', 3:r'initial $\mu$ 3D', 4:r'initial $\sigma$ 3D'
-    #                             ,5:r'$\mu$ Fit 3D', 6:r'$\sigma$ Fit 3D', 7:'mean 3D', 8:'median 3D', 9:'mode 3D'
-    #                             ,10:r'$\mu$ Fit 2D', 11:r'$\sigma$ Fit 2D', 12:'mean 2D', 13:'median 2D', 14:'mode 2D'})
-
-    # # res = dfParameters.to_csv('chondrules 2D-3D distributions results file.csv')
-
-    # with st.expander('Results Table'):
-    #     st.dataframe(dfParameters)
+    utils.func.model_plot(max_chd_size_own_model, sel_mu3D_own_model, df_results_own)
 
 
 with tab3:
--- utils/func.py
+++ utils/func.py
@@ -1,7 +1,12 @@
+import streamlit as st
 import numpy as np
 import pandas as pd
 from scipy import stats
 import random
+import matplotlib.pyplot as plt
+
+def test_func(a):
+    st.write(a)
 
 ##-------------------------------------------------------------##
 ##--- Make the 3D Chondrule List ------------------------------##
@@ -49,3 +54,69 @@ def calcMuSigma(sample):
     shape, loc, scale = stats.lognorm.fit(sample, floc=0) # hold location to 0 while fitting
     m, s = np.log(scale), shape  # mu, sigma
     return (m, s)
+
+
+##-----------------------##
+##--- The 2D/3D model ---##
+##-----------------------##
+
+def run_model(mu3DList, iniSigma_List, minChdSize_range, maxChdSize_range, numberOfChondrules, zAxisLength):
+    tot_loops = len(maxChdSize_range) * len(mu3DList) * len(iniSigma_List) * len(minChdSize_range)
+    model_progress_bar = st.progress(0)
+    loop_counter = 0
+    
+    parameters = []
+    for maxChdSize in maxChdSize_range:
+        for mu3D in mu3DList:
+            # the minimum chondrule diameters as well as the sigma of the chondrule size distribution are varied
+            for iniSigma in iniSigma_List:
+                # clear_output(wait=True) # for progress indicator, which seems not to be working in streamlit
+                # st.write(f'max Chd Size: {maxChdSize}')
+                # st.write(f'mu3D: {mu3D}')
+                # st.write(f'remaining iniSigma loops:  {len(iniSigma_List)-loop_counter}')
+                dfChdList = chd3DList(numberOfChondrules, zAxisLength, mu3D, iniSigma) # noc, zAxisLen, mu, sigma
+                
+                for minChdSize in minChdSize_range:
+                    filDiameter = (dfChdList['Chd Diameter'] > minChdSize) & (dfChdList['Chd Diameter'] < maxChdSize)
+                    dfChdList = dfChdList[filDiameter]
+                    appChdDiameterList = sectionedChd(0.5 * zAxisLength, dfChdList)
+        
+                    muFit3D, SigmaFit3D = calcMuSigma(dfChdList['Chd Diameter'])
+                    muFit2D, sigmaFit2D = calcMuSigma(appChdDiameterList)
+        
+                    parameters3D = [muFit3D, SigmaFit3D, np.e**(muFit3D + .5 * SigmaFit3D**2), np.e**muFit3D, np.e**(muFit3D - SigmaFit3D**2)]   # 3D: mu, sigma, mean, median, mode
+                    parameters2D = [muFit2D, sigmaFit2D, np.e**(muFit2D + .5 * sigmaFit2D**2), np.e**muFit2D, np.e**(muFit2D - sigmaFit2D**2)]
+                    parameters.append([len(appChdDiameterList), minChdSize, maxChdSize, mu3D, iniSigma] + parameters3D + parameters2D)
+                    loop_counter+=1
+                    model_progress_bar.progress(loop_counter/tot_loops, text='calculating – this may take some time')
+    model_progress_bar.empty()
+    st.session_state.dfParameters = pd.DataFrame(parameters)
+
+    st.session_state.dfParameters = st.session_state.dfParameters.rename(columns = {0:'Nr. of sect. Chd', 1:'min. Chd Diameter', 2:'max. Chd Diameter', 3:r'initial $\mu$ 3D', 4:r'initial $\sigma$ 3D'
+                                ,5:r'$\mu$ Fit 3D', 6:r'$\sigma$ Fit 3D', 7:'mean 3D', 8:'median 3D', 9:'mode 3D'
+                                ,10:r'$\mu$ Fit 2D', 11:r'$\sigma$ Fit 2D', 12:'mean 2D', 13:'median 2D', 14:'mode 2D'})
+
+
+##--------------------------------##
+##--- Plotting the 2D/3D model ---##
+##--------------------------------##
+
+def model_plot(max_chd_size, sel_mu3D, df_results):
+    fil = (df_results['max. Chd Diameter'] == max_chd_size) & (df_results['initial $\mu$ 3D'] == sel_mu3D)
+    dfParameters = df_results[fil]
+
+    filBlue = dfParameters['mean 2D'] <= dfParameters['mean 3D']  # 2D < 3D
+    filOrange = dfParameters['mean 2D'] > dfParameters['mean 3D']  # 2D > 3D
+    
+    # Clear previous plots
+    plt.clf()
+
+    plt.scatter(dfParameters[filBlue][r'initial $\sigma$ 3D'], dfParameters[filBlue]['min. Chd Diameter'], color = 'royalblue', s = 100)
+    plt.scatter(dfParameters[filOrange][r'initial $\sigma$ 3D'], dfParameters[filOrange]['min. Chd Diameter'], color = 'sandybrown', s = 100)
+
+    plt.scatter([.53, .46, .44],[90, 180, 245], color = 'black', edgecolors= 'white', linewidths = 3, s = 250)
+
+    plt.xlabel(r'$\sigma$ of the 3D distribution')
+    plt.ylabel('minimum sphere diameter (µm)')
+
+    return st.pyplot(plt)
